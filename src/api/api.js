2818a5e600ae0795a6080949233e2c873a3ad9f1
alfredjava <alfredfis@gmail.com>
2026-01-16 23:33:49 -0500

    varial url back

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+// Render inyectará la URL automáticamente si configuramos la variable
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1';
 // Instancia de axios configurada para tu Quarkus
 export const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
